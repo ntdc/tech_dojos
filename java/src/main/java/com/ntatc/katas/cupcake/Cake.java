8b4769af2cf169d98a4b738c94d1c20794783b46
yoann <yoann.van.den.boom@accenture.com>
2022-10-21 10:58:49 +0200

    Decorator solution for Price method

--- java/src/main/java/com/ntatc/katas/cupcake/Cake.java
+++ java/src/main/java/com/ntatc/katas/cupcake/Cake.java
@@ -1,5 +1,10 @@
 package com.ntatc.katas.cupcake;
 
+import java.math.BigDecimal;
+
 public interface Cake {
     String name();
+    default BigDecimal price() {
+        return new BigDecimal("0.0");
+    }
 }
